5b5b42ec3756ce63f373e056f09ad5bbd6ae4e61
LuisOT25 <delatorreluis81@gmail.com>
2023-03-22 15:56:54 -0600

    creacion de los components projects y certificate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PresentacionComponent } from './presentacion/presentacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: PresentacionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CertificatesComponent } from './certificates/certificates.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PresentacionComponent
+    PresentacionComponent,
+    ProjectsComponent,
+    CertificatesComponent
   ],
   imports: [
     BrowserModule,
